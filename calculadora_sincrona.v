56d41389c4e56722eb0624125f130aa2b7c02328
KaioMenezesB <kaio_borduque@hotmail.com>
2025-04-30 18:10:08 -0300

    Atividade finalizada

--- calculadora_sincrona.v
+++ calculadora_sincrona.v
@@ -1,9 +1,40 @@
 module calculadora_sincrona(
     input [7:0] entrada,
     input [2:0] codigo,
-    output reg [7:0] saida
+    output reg [7:0] saida,
+    input clk, // precisei adicionar para conseguir rodar o codigo
+    input rst // precisei adicionar para conseguir rodar o codigo
 );
-    
-//insira seu código aqui
+
+    reg [7:0] acumulador;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            acumulador <= 8'b0;
+            saida      <= 8'b0;
+        end else begin
+            saida <= 8'b0;
+            case (codigo)
+                3'b000: begin
+                    saida <= entrada;
+                end
+                3'b001: begin
+                    acumulador <= acumulador + entrada;
+                    saida      <= 8'b0;
+                end
+                3'b010: begin
+                    acumulador <= acumulador - entrada;
+                    saida      <= 8'b0;
+                end
+                3'b011: begin
+                    saida <= acumulador;
+                end
+                default: begin
+                    acumulador <= acumulador;
+                    saida      <= 8'b0;
+                end
+            endcase
+        end
+    end
 
 endmodule
